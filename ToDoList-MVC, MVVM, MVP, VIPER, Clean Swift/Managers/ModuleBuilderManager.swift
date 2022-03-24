134a78d71d16f1b3542f2c0e67523c0b63f7e709
Zorigto Badmain <badmainzzz@mail.ru>
2022-03-24 19:28:03 +0900

    added main screen

---
+++ ToDoList-MVC, MVVM, MVP, VIPER, Clean Swift/Managers/ModuleBuilderManager.swift
@@ -0,0 +1,8 @@
+//
+//  ModuleBuilderManager.swift
+//  ToDoList-MVC, MVVM, MVP, VIPER, Clean Swift
+//
+//  Created by Зоригто Бадмаин on 24.03.2022.
+//
+
+import Foundation
---
+++ ToDoList-MVC, MVVM, MVP, VIPER, Clean Swift/Modules/Main/ArhitectureModel.swift
@@ -0,0 +1,8 @@
+//
+//  ArhitectureModel.swift
+//  ToDoList-MVC, MVVM, MVP, VIPER, Clean Swift
+//
+//  Created by Зоригто Бадмаин on 24.03.2022.
+//
+
+import Foundation
